2ab94f71facd9f5fcf012afa5747ca376e974873
bkeuria1 <benkeurian@gmail.com>
2023-12-11 11:53:42 -0500

    First commit

---
+++ app/customers/page.tsx
@@ -0,0 +1,5 @@
+const Page = ()=>{
+    return <p>Welcome To Customr page</p>
+}
+
+export default Page
---
+++ app/invoices/page.tsx
@@ -0,0 +1,5 @@
+const Page = ()=>{
+    return <p>Welcome to invoices</p>
+}
+
+export default Page
